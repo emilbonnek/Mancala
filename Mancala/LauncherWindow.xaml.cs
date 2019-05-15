56adc02eb57f474e2042eea1bfe7d9f3cd1878f3
KenyanJesus <s165249@student.dtu.dk>
2019-05-16 00:19:14 +0200

    Arbejd på launcer, desværre i stykker lige nu

--- Mancala/LauncherWindow.xaml.cs
+++ Mancala/LauncherWindow.xaml.cs
@@ -25,10 +25,13 @@ namespace Mancala
         private Player player0;
         private Player player1;
         private Gamestate state = new Gamestate(0, new[] { 6, 6, 6, 6, 6, 6, 0 }, new[] { 6, 6, 6, 6, 6, 6, 0 });
-        private Game game;
 
         private String name1;
         private String name2;
+        private String inputName1;
+        private String inputName2;
+
+        private bool alreadyFocused;
 
         public LauncherWindow()
         {
@@ -42,21 +45,39 @@ namespace Mancala
             name2 = _Name2.Text;
 
             // Set a variable to the Documents path.
-            string docPath = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+            string projectDirectory = Directory.GetParent(Environment.CurrentDirectory).Parent.FullName;
 
-            //Write to files
-            using (StreamWriter outputFile = new StreamWriter(System.IO.Path.Combine(docPath, "Name1.txt")))
+            // Write to files
+            using (StreamWriter outputFile = new StreamWriter(System.IO.Path.Combine(projectDirectory, "Name1.txt")))
             {
                 outputFile.WriteLine(name1);
             }
 
-            using (StreamWriter outputFile = new StreamWriter(System.IO.Path.Combine(docPath, "Name2.txt")))
+            using (StreamWriter outputFile = new StreamWriter(System.IO.Path.Combine(projectDirectory, "Name2.txt")))
             {
                 outputFile.WriteLine(name2);
             }
 
+            // Set variables for path
+            inputName1 = System.IO.Path.Combine(projectDirectory, "Name1.txt");
+            inputName2 = System.IO.Path.Combine(projectDirectory, "Name2.txt");
+            
+            // Read from files
+            using (StreamReader sr = new StreamReader(inputName1))
+            {
+                inputName1 = sr.ReadToEnd();
+            }
 
-            var newMyWindow2 = new GameWindow(game);
+            using (StreamReader sr = new StreamReader(inputName2))
+            {
+                inputName2 = sr.ReadToEnd();
+            }
+
+
+            Console.WriteLine(player0);
+            Console.WriteLine(player1);
+
+            var newMyWindow2 = new GameWindow(state, player0, player1);
             newMyWindow2.Show();
             this.Hide();
             newMyWindow2.Closed += new EventHandler(GameWindow_Closed);
@@ -70,19 +91,61 @@ namespace Mancala
         private void _RadioPlayer_Checked(object sender, RoutedEventArgs e)
         {
 
-            if (_RadioPlayer.IsChecked == true)
+          /*  if (_RadioPlayerLeft.IsChecked == true && _RadioPlayerRight.IsChecked == true)
             {
-                player0 = new HumanPlayer("Player", 0);
-                player1 = new HumanPlayer("Player", 1);
-                game = new Game(state, player0, player1);
+                player0 = new HumanPlayer(inputName1, 0);
+                player1 = new HumanPlayer(inputName2, 0);
+            }
 
-            }
-            else if (_RadioAI.IsChecked == true)
+            else if (_RadioAILeft.IsChecked == true && _RadioPlayerRight.IsChecked == true)
             {
-                player0 = new AIPlayer("AI", 0);
-                player1 = new HumanPlayer("Player", 1);
-                game = new Game(state, player0, player1);
+                player0 = new AIPlayer("AI", 1);
+                player1 = new HumanPlayer(inputName2, 0);
             }
+
+            else if (_RadioAIRight.IsChecked == true && _RadioPlayerLeft.IsChecked == true)
+            {
+                player0 = new HumanPlayer(inputName1, 0);
+                player1 = new AIPlayer("AI", 1);
+            }
+
+            else if (_RadioAILeft.IsChecked == true && _RadioAIRight.IsChecked == true)
+            {
+                player0 = new AIPlayer("AI", 1);
+                player1 = new AIPlayer("AI", 1);
+            }
+*/            
+        }
+
+        //Textbox text disapear/reappear
+        private void _Name1_GotKeyboardFocus(object sender, KeyboardFocusChangedEventArgs e)
+        {
+            TextBox box = sender as TextBox;
+            if (!alreadyFocused && box.Text == "Player 1 Name")
+            {
+                box.Text = String.Empty;
+                alreadyFocused = true;
+            }
+        }
+
+        private void _Name1_LostKeyboardFocus(object sender, KeyboardFocusChangedEventArgs e)
+        {
+            alreadyFocused = false;
+        }
+
+        private void _Name2_GotKeyboardFocus(object sender, KeyboardFocusChangedEventArgs e)
+        {
+            TextBox box = sender as TextBox;
+            if (!alreadyFocused && box.Text =="Player 2 Name")
+            {
+                box.Text = String.Empty;
+                alreadyFocused = true;
+            }
+        }
+
+        private void _Name2_LostKeyboardFocus(object sender, KeyboardFocusChangedEventArgs e)
+        {
+            alreadyFocused = false;
         }
     }
 }
